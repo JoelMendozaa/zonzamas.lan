35d0d9c7d751e103781601bd0ed26287df63e623
Joel <yowismr@gmail.com>
2024-10-01 13:47:13 +0100

    Crear formulario en index.php

--- index.php
+++ index.php
@@ -3,11 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pruebac</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Index</title>
 </head>
 <body>
+    <h1 class="estilo">
+        Ingresa tu nombre
+    </h1>
+    <form action="" method="post" class="usuario">
+        Nombre: <input type="text" name="nombre" placeholder="Tu nombre" class="input">
+        <input type="submit" class="button">
+    </form>
+    
     <?php
-        echo ("Prueba")
+
+    if(isset($_POST["nombre"])){    // isset($_POST["nombre"]) verifica si el nombre es enviado a través del formulario  
+        $nombre = $_POST["nombre"];
+
+        if(strlen($nombre) < 5){        // Indica un minimo de caracteres
+            echo "Debe tener minimo 5 caracteres";
+        } else {
+            echo "¡Hola, $nombre!";
+        }
+    }
     ?>
+
 </body>
 </html>
