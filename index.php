50693dbed55f51a91d9483629d6bd1e7f625e3f2
Joel <yowismr@gmail.com>
2024-10-02 11:38:53 +0100

    Restauracion index.php

--- index.php
+++ index.php
@@ -7,25 +7,8 @@
     <title>Index</title>
 </head>
 <body>
-    <h1 class="estilo">
-        Ingresa tu nombre
-    </h1>
-    <form action="" method="post" class="usuario">
-        Nombre: <input type="text" name="nombre" placeholder="Tu nombre" class="input">
-        <input type="submit" class="button">
-    </form>
-    
-    <?php
-
-    if(isset($_POST["nombre"])){    // isset($_POST["nombre"]) verifica si el nombre es enviado a través del formulario  
-        $nombre = $_POST["nombre"];
-
-        if(strlen($nombre) < 5){        // Indica un minimo de caracteres
-            echo "Debe tener minimo 5 caracteres";
-        } else {
-            echo "¡Hola, $nombre! bienvenido";
-        }
-    }
+<?php
+        echo("Prueba")
     ?>
 
 </body>
