e7a3417ead60d3f9d7572b39708e84b2a4e676d8
Joel <yowismr@gmail.com>
2024-10-02 11:39:31 +0100

    Creacion y desarrollo de formulario.php

---
+++ formulario.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Formulario</title>
+</head>
+<body>
+    <h1 class="estilo">
+        Ingresa tu nombre
+    </h1>
+    <form action="formulario.php" method="post" value="<? echo $_POST['nombre'];?>" class="usuario">
+        Nombre: <input type="text" name="nombre" placeholder="Tu nombre" class="input">
+        <input type="submit" class="button">
+    </form>
+    
+    <?php
+
+    if(isset($_POST["nombre"])){    // isset($_POST["nombre"]) verifica si el nombre es enviado a través del formulario  
+        $nombre = $_POST["nombre"];
+
+        if(strlen($nombre) < 5){        // Indica un minimo de caracteres
+            echo "Debe tener minimo 5 caracteres";
+        } else {
+            echo "¡Hola, $nombre! bienvenido";
+        }
+    }
+    ?>
+
+
+</body>
+</html>
