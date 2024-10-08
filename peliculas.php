6a14888c4bf868ecd508d4e81791872987346a39
Joel <yowismr@gmail.com>
2024-10-08 13:42:50 +0100

    añadiendo peliculas.php

---
+++ peliculas.php
@@ -0,0 +1,47 @@
+<?php
+$peliculas = [
+    [
+        "titulo" => "El Señor de los Anillos: La Comunidad del Anillo",
+        "director" => "Peter Jackson",
+        "anio" => 2001,
+        "genero" => "Fantasía",
+        "poster" => "#"
+    ],
+    [
+        "titulo" => "Inception",
+        "director" => "Christopher Nolan",
+        "anio" => 2010,
+        "genero" => "Ciencia Ficción",
+        "poster" => "#"
+    ],
+    [
+        "titulo" => "El Conjuro",
+        "director" => "James Wan",
+        "anio" => 2013,
+        "genero" => "Terror",
+        "poster" => "#"
+    ],
+    [
+        "titulo" => "Star Wars: Episodio III - La venganza de los Sith",
+        "director" => "George Lucas",
+        "anio" =>  2005,
+        "genero" => "Fantasia",
+        "poster" => "#"
+    ],
+    [
+        "titulo" => "Cruella",
+        "director" => "Craig Gillespie",
+        "anio" => 2021,
+        "genero" => "Ficción Criminal",
+        "poster" => "#"
+    ],
+    [
+        "titulo" => "Interstellar",
+        "director" => "Christopher Nolan",
+        "anio" => 2014,
+        "genero" => "Ciencia Ficción",
+        "poster" => "#"
+    ],
+    // Agrega al menos 4 películas más con información similar
+];
+?>
